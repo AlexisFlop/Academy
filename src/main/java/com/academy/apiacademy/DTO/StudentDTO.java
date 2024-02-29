e289ed33a5829f4fc9b8e68ef8725679a4638b56
AlexisFlop <henryalexislopez@gmail.com>
2024-02-29 00:03:51 -0600

    Creating StudentDTO and Student model

---
+++ src/main/java/com/academy/apiacademy/DTO/StudentDTO.java
@@ -0,0 +1,41 @@
+package com.academy.apiacademy.DTO;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class StudentDTO {
+    private Integer idUser;
+
+    @NotBlank
+    @NotNull
+    @NotEmpty
+    private String firstName;
+
+    @NotBlank
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @NotBlank
+    @NotNull
+    @NotEmpty
+    private String dni;
+
+    @NotNull
+    @Min(1)
+    @Max(100)
+    private Integer age;
+}
---
+++ src/main/java/com/academy/apiacademy/Models/Student.java
@@ -0,0 +1,36 @@
+package com.academy.apiacademy.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Student {
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idUser;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String dni;
+
+    @Column(nullable = false)
+    private Integer age;
+}
