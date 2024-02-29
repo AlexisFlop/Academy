c2bfd1f6bab59d270ad9bc430767a2265e5c5cec
AlexisFlop <henryalexislopez@gmail.com>
2024-02-29 00:04:29 -0600

    Creating iStudentRepository, iGenericRespository and GenericResponse

---
+++ src/main/java/com/academy/apiacademy/Repositories/iStudentRepository.java
@@ -0,0 +1,7 @@
+package com.academy.apiacademy.Repositories;
+
+import com.academy.apiacademy.Models.Student;
+
+public interface iStudentRepository extends iGenericRepository<Student, Integer>{
+    
+}
