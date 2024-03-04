80e535e149e4f66eaab0e770ba3173bb6bb9c500
AlexisFlop <henryalexislopez@gmail.com>
2024-03-04 17:51:41 -0600

    Adding function to get Students by session

--- src/main/java/com/academy/apiacademy/Services/service_implementations/TuitionImplementation.java
+++ src/main/java/com/academy/apiacademy/Services/service_implementations/TuitionImplementation.java
@@ -1,8 +1,15 @@
 package com.academy.apiacademy.Services.service_implementations;
 
-import org.springframework.stereotype.Service;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.*;
+import java.util.stream.Stream;
 
+import org.springframework.stereotype.Service;
+import com.academy.apiacademy.Models.Student;
 import com.academy.apiacademy.Models.Tuition;
+import com.academy.apiacademy.Models.TuitionDetail;
 import com.academy.apiacademy.Repositories.iGenericRepository;
 import com.academy.apiacademy.Repositories.iTuitionRepository;
 import com.academy.apiacademy.Services.iTuitionService;
@@ -18,5 +25,17 @@ public class TuitionImplementation extends CRUDimplementation<Tuition, Integer> 
             @Override
             protected iGenericRepository<Tuition, Integer> getRepository() {
                 return repository;
+            }
+
+            @Override
+            public Map<String, List<Student>> getStudentsBySession() {
+                Stream<Tuition> tuitions = repository.findAll().stream();
+                Stream<List<TuitionDetail>> tuitionDetails = tuitions.map(Tuition::getTuitionDetails);
+
+                Stream<TuitionDetail> tuitionDetail = tuitionDetails.flatMap(Collection::stream);
+                Map<String,List<Student>> studentsBySession = tuitionDetail.collect(groupingBy(d -> d.getSession().getName(),
+                mapping(TuitionDetail::getTuition, mapping(Tuition::getStudent, toList()))));
+
+                return studentsBySession;
             }  
 }
