997587f4219b1c96e385af6b007073a2fa80a0ad
AlexisFlop <henryalexislopez@gmail.com>
2024-03-04 10:45:03 -0600

    Creating Exception Hanlder, but i am stupid bc i created at development branch and not at feature branch :(

---
+++ src/main/java/com/academy/apiacademy/Exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.academy.apiacademy.Exceptions;
+
+import java.net.URI;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ModelNotFoundException.class)
+    public ProblemDetail handleModelNotFoundException(ModelNotFoundException exception, WebRequest request) {
+        ProblemDetail problemDetdail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+        problemDetdail.setTitle("Model not found exception");
+        problemDetdail.setType(URI.create(request.getContextPath()));
+        problemDetdail.setProperty("code", "404");
+        problemDetdail.setProperty("message", "NOT-FOUND");
+
+        return problemDetdail;
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, WebRequest request) {
+        ProblemDetail problemDetdail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+        problemDetdail.setTitle("Method argument not valid exception");
+        problemDetdail.setType(URI.create(request.getContextPath()));
+        problemDetdail.setProperty("code", "400");
+        problemDetdail.setProperty("message", "BAD-REQUEST");
+
+        return problemDetdail;
+    }
+
+    /*@ExceptionHandler(Exception.class)
+    public ProblemDetail handleException(Exception exception, WebRequest request) {
+        ProblemDetail problemDetdail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
+        problemDetdail.setTitle("Internal server error");
+        problemDetdail.setType(URI.create(request.getContextPath()));
+        problemDetdail.setProperty("code", "500");
+        problemDetdail.setProperty("message", "INTERNAL-SERVER-ERROR");
+
+        return problemDetdail;
+    }   */
+}
